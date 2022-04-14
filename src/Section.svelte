d175c27d36e375eb9c8c22f853fc0f48b080505c
yassibra <tahriyassinepro@gmail.com>
2022-04-14 10:16:34 +0200

    1 file modified

--- src/Section.svelte
+++ src/Section.svelte
@@ -2,9 +2,9 @@
 <div class="py-12 bg-white">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="lg:text-center">
-      <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
-      <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">A better way to send money</p>
-      <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
+      <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Bot Telegram</h2>
+      <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Le  moyen le plus simple de creer un bot Telegram</p>
+      <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto"> Avec TelegramGenius, créer un bot Telegram n'a jamais été aussi facile.  </p>
     </div>
 
     <div class="mt-10">
@@ -17,9 +17,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
               </svg>
             </div>
-            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</p>
+            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Simplissime</p>
           </dt>
-          <dd class="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          <dd class="mt-2 ml-16 text-base text-gray-500">Telegram Genius propose une interface simple d'utilisation. Il n'est maintenant plus nécessaire d'être developpeur pour créer un bot Telegram. </dd>
         </div>
 
         <div class="relative">
@@ -30,9 +30,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
               </svg>
             </div>
-            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
+            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Genialissime</p>
           </dt>
-          <dd class="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          <dd class="mt-2 ml-16 text-base text-gray-500">Pré-visualisez en direct les fonctionnalités que vous voulez apporter à votre Bot. Créer un bot Telegram n'a jamais été autant un plaisir.</dd>
         </div>
 
         <div class="relative">
@@ -43,9 +43,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
               </svg>
             </div>
-            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
+            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Une réactivité sans équivalence</p>
           </dt>
-          <dd class="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          <dd class="mt-2 ml-16 text-base text-gray-500">Votre bot fournira des réponses instannées. Après la confirmation des changements à réaliser, TelegramGenius s'engage à mettre à jour votre bot en 1h.   </dd>
         </div>
 
         <div class="relative">
