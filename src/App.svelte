aa8d2e8b74d56d942f389c816bc9f1fed5df91ac
yassibra <tahriyassinepro@gmail.com>
2022-04-14 12:26:03 +0200

    2 files modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Home from '../src/Home.svelte';
   import Connexion from '../src/Connexion.svelte';
+  import NotFound from '../src/NotFound.svelte';
   import Router from 'svelte-spa-router';
 
  
@@ -9,7 +10,7 @@
 <Router routes={{
   '/connexion': Connexion,
   '/': Home,
-  
+  '*': NotFound
 }}
 />
 <hr>
